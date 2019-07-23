a734781766f236f4702e4b4bdc9fe0964eb2e906
webxingkong <xuanxing1314@qq.com>
2019-07-23 14:31:30 +0800

    commit

---
+++ precache-manifest.91720071619337afa2bdf775d3a296fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a11cfa9bcc6ed9468ebe8fc8b84d6d4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9bbbf73d3ee42a3819a3",
+    "url": "./static/css/2.bbec44a6.chunk.css"
+  },
+  {
+    "revision": "58cc491cda01aee32b80",
+    "url": "./static/css/main.ddb76418.chunk.css"
+  },
+  {
+    "revision": "9bbbf73d3ee42a3819a3",
+    "url": "./static/js/2.52ca2904.chunk.js"
+  },
+  {
+    "revision": "58cc491cda01aee32b80",
+    "url": "./static/js/main.fab113d7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
